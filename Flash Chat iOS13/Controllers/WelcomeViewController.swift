2f2ac605001b4bb1f41473845b97513a0b943d06
Saulo <saulo.monteiro.dev@gmail.com>
2023-07-19 11:26:09 -0300

    fist commit

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,4 +1,4 @@
-//
+        //
 //  WelcomeViewController.swift
 //  Flash Chat iOS13
 //
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import CLTypingLabel
+
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = K.appName;
+        
+        
     }
     
 
 }
+    
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Saulo on 19/07/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
